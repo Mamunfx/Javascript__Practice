e077be6ea183c1b92da0e770c496972672c2c686
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-26 17:36:15 +0000

    practice 01

--- 1_practice.js
+++ 1_practice.js
@@ -5,3 +5,13 @@ function triArea(base,hieght) {
 }
 
 console.log("Area of triangle is : "+triArea(10,30));
+
+
+
+/// Maximum edge of a triangle :--
+
+function nextEdge(side1,side2) {
+    return ((side1 + side2) - 1);
+}
+
+console.log("Next edge is : "+nextEdge(10,12));
